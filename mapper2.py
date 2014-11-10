61b4e310b32d1bbdce7b3259b81066e7451feae3
jerryE <bce@lists.berkeley.edu>
2014-11-09 21:45:03 -0800

    single feature test

---
+++ mapper2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# identity mappery
+import sys
+for line in sys.stdin:
+    line = line.strip()
+    ids = line.split('\t')
+    print '%s\t%s\t%s\t%s' % (ids[0], ids[1], ids[2], ids[3])
+
