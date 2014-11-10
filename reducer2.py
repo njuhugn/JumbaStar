15a4095dfbab0f54709cf593441f5964190404e1
jerryE <bce@lists.berkeley.edu>
2014-11-09 21:45:56 -0800

    single feature test

---
+++ reducer2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# this reducer aggregate all the instances with impression <= 20 and leave
+# other instance unchanged
+
+import sys
+
+sep_click = 0
+sep_impression = 0
+
+for line in sys.stdin:
+    line = line.strip()
+    items = line.split('\t')
+    feature = items[0] 
+    if int(items[2]) <= 20:
+        sep_click = sep_click + int(items[3])
+        sep_impression = sep_impression + int(items[2])
+    else:
+        print '%s\t%s\t%s\t%s' % (feature, "ad", items[2], items[3])
+
+print '%s\t%s\t%s\t%s' % ("impression<=10", "ad", sep_impression, sep_click)
+
