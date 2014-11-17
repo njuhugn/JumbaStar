a33a1cce973c82b1bb656bfd9561a7e99def244a
clickpn <clickpn@users.noreply.github.com>
2014-11-16 20:37:10 -0800

    decision tree R code

---
+++ Decision-Tree/decision tree.R
@@ -0,0 +1,31 @@
+#### raprt
+library(rpart)
+fit <- rpart(response ~ click+ctr,
+             method="class", data=output)
+
+printcp(fit) # display the results 
+plotcp(fit) # visualize cross-validation results 
+summary(fit) # detailed summary of splits
+
+# plot tree 
+plot(fit, uniform=TRUE)
+text(fit, use.n=TRUE, all=TRUE, cex=.6)
+post(fit, file = "~/Desktop/tree.ps", 
+     title = "Classification Tree for Kyphosis")
+
+#### party
+############# use library party
+library(party)
+str(iris)
+str(output)
+myFormula <- response ~ imp + ctr
+output_ctree <- ctree(myFormula, data=output)
+# check the prediction
+table(predict(output_ctree), output$response)
+print(output_ctree)
+plot(output_ctree)
+plot(output_ctree, type="simple")
+
+# predict on test data
+testPred <- predict(iris_ctree, newdata = testData)
+table(testPred, testData$Species)
