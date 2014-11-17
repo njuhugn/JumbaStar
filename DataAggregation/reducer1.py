c579dc1233352aa2e9303f4eed1827050de84d5f
chenjiajunjerry <chenjiajunjerry@berkeley.edu>
2014-11-17 14:16:19 -0800

    Create reducer1.py

---
+++ DataAggregation/reducer1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# this reducer return each unqiue feature \t sum of impression \t sum of click
+import sys
+
+current_value = None
+current_name = None
+click = 0
+impression = 0
+for line in sys.stdin:
+    line = line.strip()
+    items = line.split('\t')
+    feature_value = items[1]
+    
+    if current_value == feature_value:
+        click = click + int(items[3])
+        impression = impression + int(items[2])
+    else:
+        if current_value and current_name:
+            print '%s\t%s\t%s\t%s' % (current_name, current_value,\
+                                      impression, click)
+        current_value = feature_value
+        current_name = items[0]
+        click = int(items[3])
+        impression = int(items[2])
+            
+print '%s\t%s\t%s\t%s' % (current_name, current_value, impression, click)
+### By Jiajun Chen
