20bf40840305057c9fcbe249c69fa43da696deeb
chenjiajunjerry <chenjiajunjerry@berkeley.edu>
2014-11-17 14:17:01 -0800

    Create mapper2.py

---
+++ DataAggregation/mapper2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# identity mapper
+import sys
+for line in sys.stdin:
+    line = line.strip()
+    items = line.split('\t')
+    print '%s\t%s\t%s\t%s' % (items[0], items[1], items[2], items[3])
+
+### By Jiajun Chen 
