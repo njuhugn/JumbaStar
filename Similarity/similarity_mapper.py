68b86f7c398fbea30b22936b516a5c68eb0823c7
clickpn <clickpn@users.noreply.github.com>
2014-11-16 19:47:37 -0800

    mapper output format is quaryID/tAdID

---
+++ Similarity/similarity_mapper.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import sys
+"""
+        1.
+        2.
+        3. Click
+        4. Impression
+        5. DisplayURL
+        6. AdID
+        7. AdvertiserID
+        8. Depth
+        9. Position
+        10. QueryID
+        11. KeywordID
+        12. TitleID
+        13. DescriptionID
+        14. UserID
+"""
+for line in sys.stdin:
+    line = line.strip()
+    lst = line.split("\t")
+    try:
+        if len(lst) == 14:
+            print '%s\t%s' % (lst[9], lst[5]) # remember to change index
+    except Exception:
+        continue
