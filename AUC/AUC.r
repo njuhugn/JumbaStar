b6c78458683c4a1d8974d82921a824b294f02448
victoryuhao <victor.yuhao@gmail.com>
2014-11-16 21:22:42 -0800

    Create AUC.r

---
+++ AUC/AUC.r
@@ -0,0 +1,15 @@
+Author: Victor Jiang
+
+
+install.packages('AUC')
+library(AUC)
+help(accuracy)
+
+data(churn)
+churn$labels
+x=accuracy(churn$predictions,churn$labels)
+auc(x)
+out <- read.delim("~/Downloads/out.txt", header=F)
+View(out)
+x[x[,5]>1,5]=1
+y=accuracy(x$V3,factor(x$V5))
