8388b963e838b3cff474a05b4d7abb2e650c8d77
clickpn <clickpn@users.noreply.github.com>
2014-11-17 17:33:20 -0800

    identity mapper

---
+++ Similarity/similarity_mapper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    lst = line.split("\t")
+    try:
+        if len(lst) == 2:
+            print '%s\t%s' % (lst[0], lst[1]) # remember to change index
+    except Exception:
+        continue
+
+
