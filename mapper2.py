490fef0808e9abb9f13f2d5275bf71c874345d8f
chenjiajunjerry <chenjiajunjerry@berkeley.edu>
2014-11-17 21:43:53 -0800

    Delete mapper2.py

--- mapper2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# identity mappery
-import sys
-for line in sys.stdin:
-    line = line.strip()
-    ids = line.split('\t')
-    print '%s\t%s\t%s\t%s' % (ids[0], ids[1], ids[2], ids[3])
-
