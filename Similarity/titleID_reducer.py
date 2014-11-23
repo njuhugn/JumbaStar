834e05764d9aba5d334608376eafc0d54991bf51
clickpn <clickpn@users.noreply.github.com>
2014-11-23 15:29:05 -0800

    Update titleID_reducer.py

--- Similarity/titleID_reducer.py
+++ Similarity/titleID_reducer.py
@@ -2,10 +2,12 @@
 # author: Sida Ye, Jiajun Chen
 import sys
 
+token = None
+
 for line in sys.stdin:
     line = line.strip()
     items = line.split('\t')
-    if items[-1] == 'token':
+    if items[-1] == 'token' and len(items) == 3:
     	token = items[1]
     else:
     	print '%s\t%s' % (items[1], token)
