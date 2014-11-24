d8046876e291ab6cd49fbf979bceece8bc70fbfc
clickpn <clickpn@users.noreply.github.com>
2014-11-23 18:25:09 -0800

    combine query token reducer

---
+++ Similarity/query_reducer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#author: Sida Ye
+import sys
+
+token = None
+
+"""
+Input format: query_id /t instance /t titel_id_token
+              query_id /t query_id_token /t 'token'
+
+
+"""
+
+for line in sys.stdin:
+    line = line.strip()
+    items = line.split('\t')
+    if items[-1] == 'token':
+    	token = items[1]
+    else:
+    	print '%s\t%s\t%s' % (items[1], items[2], token)
+    		
