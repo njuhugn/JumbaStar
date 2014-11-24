836b6d1356be9bcf7d72ac2f79886aa7fcadf6eb
clickpn <clickpn@users.noreply.github.com>
2014-11-24 13:52:32 -0800

    similarity calculation
    Calculate similarity feature between queryid and title id with an identity reducer

---
+++ Similarity/similarity_mapper.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# author: Sida Ye
+import sys
+"""
+        1.
+        2.
+        3. Click
+        4. Impression
+        5. DisplayURL
+        6. AdID
+        7. AdvertiserID
+        8. Depth
+        9. Position
+        10. QueryID
+        11. KeywordID
+        12. TitleID
+        13. DescriptionID
+        14. UserID
+
+Input format: instance(list) /t title_id_token/ query_id_token
+
+"""
+for line in sys.stdin:
+    line = line.strip()
+    items = line.split('\t')
+    if len(items) == 3:
+        title_id_token = items[1].split("|")
+        query_id_token = items[2].split("|")
+        count = 0
+        for item in query_id_token:
+            if item in title_id_token:
+                count += 1
+        index = float(count) / float(len(query_id_token))
+        print '%s\t%s' % (items[0], index)
