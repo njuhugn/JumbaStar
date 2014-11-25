9f4282c226760f589e0e135f8d99f41460faa908
clickpn <clickpn@users.noreply.github.com>
2014-11-25 12:47:59 -0800

    Create relative_pos_mapper.py

---
+++ relative_pos_mapper.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# author: Sida Ye
+import sys
+"""
+        1.
+        2.
+        3. Click
+        4. Impression
+        5. DisplayURL
+        6. AdID
+        7. AdvertiserID
+        8. Depth
+        9. Position
+        10. QueryID
+        11. KeywordID
+        12. TitleID
+        13. DescriptionID
+        14. UserID
+
+Input format: instance(list) /t similarity_index
+
+"""
+for line in sys.stdin:
+    line = line.strip()
+    items = line.split('\t')
+    lst = items[0].split(',')
+    if len(items) == 2:
+        pos = int(lst[7].replace("'",""))
+        depth = int(lst[8].replace("'",""))
+        relative_pos = float((depth - pos)) / float(depth)
+        print '%s\t%s' % (items[0], relative_pos)
+        
+# use identity reducer
