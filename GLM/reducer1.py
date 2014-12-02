ce10ef95d3b74fe6198ecbb183d096d41fb9ffb1
chenjiajunjerry <chenjiajunjerry@berkeley.edu>
2014-12-01 23:18:57 -0800

    Create reducer1.py

---
+++ GLM/reducer1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import sys
+
+current_id = None
+current_pos = None
+current_dep = None
+click = 0
+impression = 0
+for line in sys.stdin:
+    line = line.strip()
+    items = line.split('\t')
+    ad_id = items[0]
+    position = items[1]
+    depth = items[2]
+    if current_id == ad_id and current_pos == position and current_dep == depth:
+        click = click + int(items[3])
+        impression = impression + int(items[4])
+    else:
+        if current_id and current_pos and current_dep:
+            print '%s\t%s\t%s\t%s\t%s' % (current_id,current_pos,current_dep,click,impression)
+        current_id = ad_id
+        current_pos = position
+        current_dep = depth
+        click = int(items[3])
+        impression = int(items[4])
+            
+print '%s\t%s\t%s\t%s\t%s' % (ad_id,position,depth,click,impression)
