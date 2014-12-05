2b57fc1b61aa0637aca119cd4c0e71d806862a16
victoryuhao <victor.yuhao@gmail.com>
2014-12-04 23:27:46 -0800

    Create reducer.py

---
+++ Models/Naive-Bayes/all/reducer.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#Author: Victor Jiang
+
+
+import sys
+from operator import itemgetter
+
+current_click = 0
+current_impression = 0
+currentkey = None
+
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    key, typeclass, click, impression = line.split('\t',3)
+    # convert count (currently a string) to int
+    try:
+        click = float(click)
+    except ValueError:
+        continue
+    try:
+        impression = float(impression)
+    except ValueError:
+        continue
+
+    if currentkey == None or currentkey == key:
+        currentkey = key 
+        current_click += click
+        current_impression += impression
+
+    else:    
+        print '%s\t%s\t%s\t%s' % (currentkey, typeclass, current_click, current_impression)
+        currentkey = key
+        current_click = click
+        current_impression = impression
+
+# do not forget to output the last word if needed!
+if currentkey == key:
+    print '%s\t%s\t%s\t%s' % (currentkey,typeclass, current_click, current_impression)
