41839cb906dc40187b6532099679829d81131568
victoryuhao <victor.yuhao@gmail.com>
2014-12-05 01:21:52 -0800

    Update mapper.py

--- Models/Naive-Bayes/similarity/mapper.py
+++ Models/Naive-Bayes/similarity/mapper.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 #Author: Victor Jiang
 
-
+#mapper.py: organize the orginal data to get the columns I am interested
+#input: ['0.0822', 'train', '0', '1', '11092342671025131053', '20883284', '26561', '2', '1', '11929585', '65174', '1000019', '505595', '0']	\t additional feature
+#output: value \t featurename \t click \t impression
 
 import sys
 import os.path
