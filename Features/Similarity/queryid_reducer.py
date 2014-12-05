3de158dbd14b1bd4e7afaaf7bfcb06482976ede7
clickpn <clickpn@users.noreply.github.com>
2014-12-05 00:33:09 -0800

    Update queryid_reducer.py

--- Features/Similarity/queryid_reducer.py
+++ Features/Similarity/queryid_reducer.py
@@ -10,7 +10,7 @@ token = None
 Input format: query_id /t instance /t titel_id_token
               query_id /t query_id_token /t 'token'
 
-
+Output format: instance /t titleID_token /t queryID_token
 """
 
 for line in sys.stdin:
